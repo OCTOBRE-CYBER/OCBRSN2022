55b717378ec111e594c45b64f9777f9102f52111
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-10-02 09:04:53 +0000

    AdressMailocbsn2022

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'abdallahfall2020@gmail.com';
+  $receiving_email_address = 'ocbsn2022@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -27,8 +27,8 @@
   
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
-    'username' => 'abdallahfall2020@gmail.com',
-    'password' => 'ahstaogzubgrgnsf',
+    'username' => 'ocbsn2022@gmail.com',
+    'password' => 'cjhisygfahtomtxf',
     'port' => '587'
   );
   
